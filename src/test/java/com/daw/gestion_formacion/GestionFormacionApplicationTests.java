12401e0067af033257babc348034caffc15a4479
Abeagom <beas.gomez.alberto@iescamas.es>
2026-02-03 16:48:53 +0100

    Estructura inicial

---
+++ src/test/java/com/daw/gestion_formacion/GestionFormacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.gestion_formacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFormacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
